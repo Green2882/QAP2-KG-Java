4d588a82a3db09a9beb7ce9850cc93a122d0eb88
Kevin Green <kevingreen@macbookair.home>
2025-09-27 17:08:43 -0230

    Added instance variables and constructor

--- task1/CreditCard.java
+++ task1/CreditCard.java
@@ -1,4 +1,16 @@
 package task1;
 
 public class CreditCard {
+
+    //Instance variables
+    private Money balance;
+    private Money creditLimit;
+    private Person owner;
+
+    // Constructor
+    public CreditCard(Person newCardHolder, Money limit) {
+
+        this.owner = newCardHolder;
+        this.creditLimit = limit;
+    }
 }
