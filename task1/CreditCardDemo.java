ff944513e6de74d0eb76ac9e17c25817ac916118
Kevin Green <kevingreen@macbookair.home>
2025-09-28 11:47:28 -0230

    Set final variables

--- task1/CreditCardDemo.java
+++ task1/CreditCardDemo.java
@@ -4,5 +4,14 @@ public class CreditCardDemo {
 
     public static void main(String[] args) {
 
+        {
+
+            final Money LIMIT = new Money(1000);
+            final Money FIRST_AMOUNT = new Money(200);
+            final Money SECOND_AMOUNT = new Money(10.02);
+            final Money THIRD_AMOUNT = new Money(25);
+            final Money FOURTH_AMOUNT = new Money(990);
+
+        }
     }
 }
