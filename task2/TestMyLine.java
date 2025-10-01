7c797644d40220e3a663e79e87d5eee5e1492ab8
Kevin Green <Greenkevin28@icloud.com>
2025-10-01 19:11:40 -0230

    Set up two lines using the two constructors from MyLine.java

--- task2/TestMyLine.java
+++ task2/TestMyLine.java
@@ -4,5 +4,12 @@ public class TestMyLine {
 
     public static void main(String[] args) {
 
+        // Create two lines using the different constructors
+        MyLine l1 = new MyLine(2, 4, 4, 6);
+
+        MyPoint p1 = new MyPoint(0, 0);
+        MyPoint p2 = new MyPoint(0, 5);
+        MyLine l2 = new MyLine(p1, p2);
+
     }
 }
