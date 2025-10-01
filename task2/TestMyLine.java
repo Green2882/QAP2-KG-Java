0dd427a6b3a25fdc7322b4fb3d9758f2a45d0f3a
Kevin Green <Greenkevin28@icloud.com>
2025-10-01 19:24:19 -0230

    Tested all getters

--- task2/TestMyLine.java
+++ task2/TestMyLine.java
@@ -1,15 +1,29 @@
 package task2;
 
+import java.util.Arrays;
+
 public class TestMyLine {
 
     public static void main(String[] args) {
 
         // Create two lines using the different constructors
-        MyLine l1 = new MyLine(2, 4, 4, 6);
+        MyLine l1 = new MyLine(1, 1, 2, 2);
 
         MyPoint p1 = new MyPoint(0, 0);
-        MyPoint p2 = new MyPoint(0, 5);
+        MyPoint p2 = new MyPoint(3, 0);
         MyLine l2 = new MyLine(p1, p2);
+
+        // Test getters
+        System.out.println("l1 begin = " + l1.getBegin());
+        System.out.println("l1 end = " + l1.getEnd());
+        System.out.println("l2 begin = " + l2.getBegin());
+        System.out.println("l2 end = " + l2.getEnd());
+        System.out.println("l1 begin X = " + l1.getBeginX());
+        System.out.println("l1 begin Y = " + l1.getBeginY());
+        System.out.println("l2 end X = " + l2.getEndX());
+        System.out.println("l2 end Y = " + l2.getEndY());
+        System.out.println("l1 begin XY = " + Arrays.toString(l1.getBeginXY()));
+        System.out.println("l2 end XY = " + Arrays.toString(l2.getEndXY()));
 
     }
 }
