82d9d1b84ba479d066875cb5e9dfcea81e83d6a2
Kevin Green <Greenkevin28@icloud.com>
2025-10-02 10:18:00 -0230

    Added getters and setters for topLeft and bottomRight

--- task3/MyRectangle.java
+++ task3/MyRectangle.java
@@ -20,4 +20,26 @@ public class MyRectangle {
         this.topLeft = new MyPoint(x1, y1);
         this.bottomRight = new MyPoint(x2, y2);
     }
+
+    // Getters and setters for topLeft
+    public MyPoint getTopLeft() {
+
+        return this.topLeft;
+    }
+
+    public void setTopLeft(MyPoint topLeft) {
+
+        this.topLeft = topLeft;
+    }
+
+    // Getters and setters for bottomRight
+    public MyPoint getBottomRight() {
+
+        return this.bottomRight;
+    }
+
+    public void setBottomRight(MyPoint bottomRight) {
+
+        this.bottomRight = bottomRight;
+    }
 }
