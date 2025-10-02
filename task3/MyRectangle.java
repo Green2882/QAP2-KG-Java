e55780dd95f4838a0fdb35b711a26203ace600e9
Kevin Green <Greenkevin28@icloud.com>
2025-10-02 10:11:14 -0230

    Added two constructors

--- task3/MyRectangle.java
+++ task3/MyRectangle.java
@@ -4,7 +4,20 @@ import task2.MyPoint;
 
 public class MyRectangle {
 
-// Instance variables
+    // Instance variables
     private MyPoint topLeft;
     private MyPoint bottomRight;
+
+    // Constructors
+    public MyRectangle(MyPoint topLeft, MyPoint bottomRight) {
+
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+    }
+
+    public MyRectangle(int x1, int y1, int x2, int y2) {
+
+        this.topLeft = new MyPoint(x1, y1);
+        this.bottomRight = new MyPoint(x2, y2);
+    }
 }
