9c01565593526335525cb12435daad60c583b39c
Kevin Green <Greenkevin28@icloud.com>
2025-10-04 18:46:32 -0230

    Added coords to buildings and created fountain

--- task4/TestCampusMap.java
+++ task4/TestCampusMap.java
@@ -1,5 +1,6 @@
 package task4;
 
+import task2.MyPoint;
 import task3.MyRectangle;
 
 public class TestCampusMap {
@@ -9,14 +10,18 @@ public class TestCampusMap {
         // Create campus map
         CampusMap campus = new CampusMap();
 
-        // Create 3 buildings 
-        MyRectangle scienceHall = new MyRectangle(0, 0, 0, 0);
-        MyRectangle library = new MyRectangle(0, 0, 0, 0);
-        MyRectangle lectureRoom1 = new MyRectangle(0, 0, 0, 0);
-        MyRectangle lectureRoom2 = new MyRectangle(0, 0, 0, 0);
-        MyRectangle lectureRoom3 = new MyRectangle(0, 0, 0, 0);
-        MyRectangle lectureRoom4 = new MyRectangle(0, 0, 0, 0);
-        MyRectangle lectureRoom5 = new MyRectangle(0, 0, 0, 0);
+        // Create 7 buildings 
+        MyRectangle scienceHall = new MyRectangle(380, 740, 620, 560);
+        MyRectangle library = new MyRectangle(700, 900, 950, 700);
+        MyRectangle lectureRoom1 = new MyRectangle(120, 360, 200, 300);
+        MyRectangle lectureRoom2 = new MyRectangle(210, 360, 290, 300);
+        MyRectangle lectureRoom3 = new MyRectangle(300, 360, 380, 300);
+        MyRectangle lectureRoom4 = new MyRectangle(165, 290, 245, 230);
+        MyRectangle lectureRoom5 = new MyRectangle(255, 290, 335, 230);
+
+        // Create a fountain
+        MyPoint p1 = new MyPoint(500, 500);
+        MyCircle fountain = new MyCircle(p1, 50);
 
     }
 }
