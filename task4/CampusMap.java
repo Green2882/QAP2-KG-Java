5ba37d268e6c9bb70d49366ef0ca2a2a7bf18dc6
Kevin Green <Greenkevin28@icloud.com>
2025-10-04 12:15:35 -0230

    Set up instance variables and no arg  contrsuctor

--- task4/CampusMap.java
+++ task4/CampusMap.java
@@ -1,9 +1,21 @@
 package task4;
 
-import task2.MyPoint;
+import java.util.ArrayList;
 import task2.MyLine;
 import task3.MyRectangle;
 
 public class CampusMap {
 
+    // Instance variables
+    private ArrayList<MyRectangle> buildings;
+    private ArrayList<MyLine> walkways;
+    private ArrayList<MyCircle> fountains;
+
+    // No arg constructor to store list of walkways, buildings, and fountains
+    public CampusMap() {
+
+        buildings = new ArrayList<>();
+        walkways = new ArrayList<>();
+        fountains = new ArrayList<>();
+    }
 }
