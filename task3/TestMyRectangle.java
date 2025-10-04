58ccdd5d92a8a437abce1de43c09a18176001911
Kevin Green <Greenkevin28@icloud.com>
2025-10-04 11:33:42 -0230

    Created rectangles and displayed original points

--- task3/TestMyRectangle.java
+++ task3/TestMyRectangle.java
@@ -6,5 +6,16 @@ public class TestMyRectangle {
 
     public static void main(String[] args) {
 
+        // Set up two rectangles with the two constrcutors
+        MyPoint p1 = new MyPoint(2, 5); // topLeft
+        MyPoint p2 = new MyPoint(7, 2); // bottomRight
+        MyRectangle r1 = new MyRectangle(p1, p2);
+        MyRectangle r2 = new MyRectangle(3, 6, 8, 3);
+
+        // Display original rectangles
+        System.out.println();
+        System.out.println(r1);
+        System.out.println(r2);
+
     }
 }
