cb42ec09e12cd4d0af8db6dcd499d1af0f86ae23
Kevin Green <Greenkevin28@icloud.com>
2025-10-04 12:34:57 -0230

    Added methods to add buildings, walkways, and fountains

--- task4/CampusMap.java
+++ task4/CampusMap.java
@@ -18,4 +18,39 @@ public class CampusMap {
         walkways = new ArrayList<>();
         fountains = new ArrayList<>();
     }
+
+    // Methods to add buildings, walkways, and fountains to matching lists
+    public MyRectangle addBuilding(MyRectangle building) {
+
+        buildings.add(building);
+        return building;
+    }
+
+    public MyLine addWalkway(MyRectangle from, MyRectangle to) {
+
+        if (!buildings.contains(from) || !buildings.contains(to)) {
+            System.out.println("Error: One or both buildings are not in the campus map.");
+            return null;
+        }
+
+        // Calculate center of from
+        int fromCenterX = (from.getTopLeftX() + from.getBottomRightX()) / 2;
+        int fromCenterY = (from.getTopLeftY() + from.getBottomRightY()) / 2;
+
+        // Calculate center of to
+        int toCenterX = (to.getTopLeftX() + to.getBottomRightX()) / 2;
+        int toCenterY = (to.getTopLeftY() + to.getBottomRightY()) / 2;
+
+        // Create walkway
+        MyLine walkway = new MyLine(fromCenterX, fromCenterY, toCenterX, toCenterY);
+
+        walkways.add(walkway);
+        return walkway;
+    }
+
+    public MyCircle addFountain(MyCircle fountain) {
+
+        fountains.add(fountain);
+        return fountain;
+    }
 }
