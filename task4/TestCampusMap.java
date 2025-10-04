b97c5beb797be6a8532227af9da9cc7459161805
Kevin Green <Greenkevin28@icloud.com>
2025-10-04 18:40:54 -0230

    Created empty campus map and 7 buildings(no cords yet)

--- task4/TestCampusMap.java
+++ task4/TestCampusMap.java
@@ -1,11 +1,22 @@
 package task4;
 
 import task3.MyRectangle;
-import task2.MyLine;
 
 public class TestCampusMap {
 
     public static void main(String[] args) {
+
+        // Create campus map
+        CampusMap campus = new CampusMap();
+
+        // Create 3 buildings 
+        MyRectangle scienceHall = new MyRectangle(0, 0, 0, 0);
+        MyRectangle library = new MyRectangle(0, 0, 0, 0);
+        MyRectangle lectureRoom1 = new MyRectangle(0, 0, 0, 0);
+        MyRectangle lectureRoom2 = new MyRectangle(0, 0, 0, 0);
+        MyRectangle lectureRoom3 = new MyRectangle(0, 0, 0, 0);
+        MyRectangle lectureRoom4 = new MyRectangle(0, 0, 0, 0);
+        MyRectangle lectureRoom5 = new MyRectangle(0, 0, 0, 0);
 
     }
 }
